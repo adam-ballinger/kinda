7fe146808f768f564bd541712b2546545cb0b3f0
Adam Ballinger <ballinger5421@gmail.com>
2022-06-12 15:47:42 -0500

    Setup server and client to post and get messages.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Farms
 
-[![Generic badge](https://img.shields.io/badge/Author-Adam Ballinger-<COLOR>.svg)](https://github.com/adam-ballinger/)]
-
 Farm management web app for learning web app development.
+
+## Author
+Adam Ballinger
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
-var server = app.listen(3000, () => {
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var messages = [
+    {name: 'Chicks', message: 41.17},
+    {name: 'Feed', message: 5.42}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    io.emit('message', req.body)
+    res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected.')
+})
+
+
+var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
 })
 
